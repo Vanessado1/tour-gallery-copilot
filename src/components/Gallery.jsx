6e3b470ca7fbe308ebac1f809328852f6f3ba9c3
Vanessa Do <xex.vanessa@gmail.com>
2025-04-26 15:13:55 -0400

    Fixed Gallery issue

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import TourCard from './TourCard';
+import TourCard from './components.TourCard';
 
 const Gallery = ({ tours }) => {
     return (
         <div className="gallery">
             {tours.map((tour) => (
-                <TourCard key={tour.id} tour={tour} />
+                <TourCard key={tour.id} {...tour} />
             ))}
         </div>
     );
